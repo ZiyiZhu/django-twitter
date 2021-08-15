1b8441a03721617d761e9aa0f61b106e8977635c
ZiyiZhu <zz2374@columbia.edu>
2021-08-15 02:03:54 -0400

    debug tool

--- twitter/settings.py
+++ twitter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_d3r#*i4hnce5b*rsh7%dh-p2r97ddrg3h5lo%c*1-gp0859a='
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.33.10', 'localhost']
-
+INTERNAL_IPS = ['10.0.2.2']
 
 # Application definition
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third party
     'rest_framework',
+    'debug_toolbar',
 
     # project apps
     'accounts',
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter.urls'
