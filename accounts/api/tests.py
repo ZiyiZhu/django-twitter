64779a32ce8395f4d581a731c565b0c4f22e5c67
ZiyiZhu <zz2374@columbia.edu>
2021-09-26 20:48:05 -0400

    22-userprofile-model-and-admin

--- accounts/api/tests.py
+++ accounts/api/tests.py
@@ -1,3 +1,4 @@
+from accounts.models import UserProfile
 from rest_framework.test import APIClient
 from testing.testcases import TestCase
 
@@ -5,6 +6,7 @@ LOGIN_URL = '/api/accounts/login/'
 LOGOUT_URL = '/api/accounts/logout/'
 SIGNUP_URL = '/api/accounts/signup/'
 LOGIN_STATUS_URL = '/api/accounts/login_status/'
+
 
 class AccountApiTests(TestCase):
     def setUp(self):
@@ -98,6 +100,7 @@ class AccountApiTests(TestCase):
         })
         # print(response.data)
         self.assertEqual(response.status_code, 400)
+
         # 测试⽤户名太⻓
         response = self.client.post(SIGNUP_URL, {
             'username': 'username is tooooooooooooooooo loooooooong',
@@ -106,10 +109,15 @@ class AccountApiTests(TestCase):
         })
         # print(response.data)
         self.assertEqual(response.status_code, 400)
+
         # 成功注册
         response = self.client.post(SIGNUP_URL, data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['user']['username'], 'someone')
+        # 验证 user profile 已经被创建
+        created_user_id = response.data['user']['id']
+        profile = UserProfile.objects.filter(user_id=created_user_id).first()
+        self.assertNotEqual(profile, None)
         # 验证⽤户已经登⼊
         response = self.client.get(LOGIN_STATUS_URL)
         self.assertEqual(response.data['has_logged_in'], True)
